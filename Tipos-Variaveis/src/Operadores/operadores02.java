8b26a3230cce7f4d306ef23cfa851d86b084a7c7
GuiSilva011 <guilherne1207@gmail.com>
2025-03-16 10:28:55 -0300

    operadores02

---
+++ Tipos-Variaveis/src/Operadores/operadores02.java
@@ -0,0 +1,19 @@
+package Operadores;
+
+public class operadores02 {
+  public static void main(String[]args){
+    int numero = 5;
+
+    //x repetição
+   
+    //numero++;
+
+    System.out.println(numero --);
+    System.out.println(numero);
+
+
+    boolean variavel = false;
+    System.out.println(!variavel);
+  }
+
+}
